a464cbca5fa8617c910dbccfb7e8ad437fb641ca
Amanda Regan <amandaregan@MandysMacMini.local>
2024-03-23 11:40:43 -0400

    add csvs describing inconsistency between files and metadata

--- dataprep.R
+++ dataprep.R
@@ -3,11 +3,15 @@ library(tidyverse)
 metadata <- read.csv(file = "IndianaNewspapersMasterMetadata.csv", header = TRUE)
 metadata <- metadata %>% filter(Transcript == TRUE)
 
+
+
 txt <- list.files(path = "txt/")
 txt <- as.data.frame(txt)
 txt$hasfile <- TRUE
 txt <- txt %>% rename("Filename" = "txt")
 
-test <- left_join(txt, metadata, by="Filename")
-unique(test$Filename)
-test$duplicate <- duplicated(test$Filename)
+Extrafiles <- txt %>% filter(!Filename %in% metadata$Filename)
+Extrametadata <- metadata %>% filter(!Filename %in% txt$Filename)
+
+metadata$duplicated <- duplicated(metadata$Filename)
+dups <- metadata %>% filter(duplicated == TRUE)
